cd36b386dd600bd725ed8a6c649e99d58a060c88
Eva van Agt <eagt3@gatech.edu>
2020-10-05 19:55:30 +0200

    added file for further processing, tried out some stuff in main, and added data files that take 7500 api requests

--- main.py
+++ main.py
@@ -1,27 +1,85 @@
 from api_requests import busestrams_get, dbstore_get, dbtimetable_get
 import pandas as pd
-
+from processing import get_all_lines, get_lines_per_busstop, get_busstops_per_line, get_timetable_per_busstop_per_line, coord_distance
+import json
 
 
 def main():
-    # Example calls (uncomment and print to see the results):
-
-    df = busestrams_get()
-
+    # Example calls from api_requests.py (uncomment and print to see the results):
+    # -----------------------
+    # df = busestrams_get()
+    # -----------------------
     # df = dbstore_get()
-
+    # -----------------------
     # retrieving the line list for the Marszałkowska stop  (busstopId = 7009) and bar number 01 (busstopNr = 01) and line 523 (line = 523)
     # df = dbtimetable_get(dict(busstopId='7009', busstopNr='01', line='523'))
-
+    # -----------------------
     # retrieving the line list for the Marszałkowska stop (busstopId = 7009) and bar number 01 (busstopNr = 01)
     # df = dbtimetable_get(dict(busstopId='7009', busstopNr='01'))
-
-
+    # -----------------------
     # retrieving all the 'zespol' (??) for the busstop 'Marysin'
     # df = dbtimetable_get(dict(name='Marysin'))
+    # -----------------------
+    # pd.set_option('display.max_columns', None)
+    # print(df)
 
+    # ---------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------
+    # SOME STUFF TO TRY OUT OTHER STUFF:
+
+    # For each line, get list of busstopId+nr on route
+
+    # Unnecessary, but whatever:
+    # get a list of all lines
+    # all_lines = get_all_lines()
+    # --> saved in all_lines.txt: 342 lines
+
+    # get dictionary of lines per busstop
+    # line_dict = get_lines_per_busstop()
+    # line_dict = dict((':'.join(k), v) for k, v in line_dict.items())
+    # with open('lines_per_busstop.json', 'w') as fh:
+    #    json.dump(line_dict, fh, sort_keys=True, indent=4)
+    # --> saved in lines_per_busstop.json
+
+    """
+    with open('lines_per_busstop.json', 'r') as fh:
+        my_dict = json.load(fh)
+    test_dict = dict()
+    for k in my_dict:
+        k_split = k.split(':')
+        new_k = (k_split[0], k_split[1])
+        test_dict[new_k] = my_dict[k]
+    busstops_per_line = get_busstops_per_line(test_dict)
+    print(busstops_per_line)
+    """
+    # timetables = get_timetable_per_busstop_per_line(busstops_per_line)
+    # print(timetables)
+
+    # QUESTION: are there any two bus stops on the same line
+    #   that are within a 2r meter distance of each other (to start: r=300)?
+    # CONCLUSION (run commented code below): YES, MANY! Even distances of just 17 meters...
+    """ 
+    all_stops = dbstore_get()
     pd.set_option('display.max_columns', None)
-    print(df)
+    print(all_stops)
+
+    for line in busstops_per_line:
+        stop_list = busstops_per_line[line]
+        permutations = [(stop_list[i], stop_list[j]) for i in range(len(stop_list)) for j in range(i+1, len(stop_list))]
+
+    for p in permutations:
+        stop1 = p[0]
+        stop2 = p[1]
+        stop1_info = all_stops.loc[(all_stops['zespol'] == stop1[0]) & (all_stops['slupek'] == stop1[1])]
+        stop2_info = all_stops.loc[(all_stops['zespol'] == stop2[0]) & (all_stops['slupek'] == stop2[1])]
+        lat1 = stop1_info['szer_geo']
+        lon1 = stop1_info['dlug_geo']
+        lat2 = stop2_info['szer_geo']
+        lon2 = stop2_info['dlug_geo']
+        dist = coord_distance((lat1, lon1), (lat2, lon2))
+        print(dist)
+    """
+    pass
 
 
 
---
+++ processing.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import numpy as np
+from math import radians, cos, sin, asin, sqrt
+import api_requests as api
+from collections import defaultdict
+
+def coord_distance(coord1, coord2):
+    """
+    CREDITS TO GEEKSFORGEEKS.ORG: https://www.geeksforgeeks.org/program-distance-two-points-earth/
+        Function that calculates and returns the distance in meters between two coordinates
+    Args:
+        coord1 (numpy.float64, numpy.float64): tuple with latitude & longitude (in that order) for the first coordinate
+        coord2 (numpy.float64, numpy.float64): tuple with latitude & longitude (in that order) for the second coordinate
+
+    Returns:
+        dist (float): distance in meters
+
+    Example call:
+        coord_distance((52.195248, 21.048823), (52.208840, 21.007883))
+    """
+
+    lon1 = radians(coord1[1])
+    lon2 = radians(coord2[1])
+    lat1 = radians(coord1[0])
+    lat2 = radians(coord2[0])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+
+    c = 2 * asin(sqrt(a))
+
+    # Radius of earth in meters. Use 3956 for miles
+    r = 6371000
+
+    return (c * r)
+
+
+def get_all_lines():
+    """Returns a set of all lines
+    (Takes some time: ~7500 api requests)
+    """
+    all_busstops = api.dbstore_get()
+    n = len(all_busstops)
+    all_lines = set()
+    zespol = all_busstops['zespol']
+    slupek = all_busstops['slupek']
+    for i in range(n):
+        print(i)
+        stop_id = zespol[i]
+        stop_nr = slupek[i]
+        lines = api.dbtimetable_get({'busstopId': str(stop_id), 'busstopNr': str(stop_nr)})
+        for j in range(len(lines)):
+            all_lines.add(lines[j])
+    return all_lines
+
+
+def get_lines_per_busstop():
+    """Returns a dictionary with busstops as keys (tuple of (id, nr)), and their lines as values
+    (Takes some time: ~7500 api requests)
+    """
+    all_busstops = api.dbstore_get()
+    n = len(all_busstops)
+    line_per_busstop_dict = dict()
+    zespol = all_busstops['zespol']
+    slupek = all_busstops['slupek']
+    for i in range(n):
+        print(i)
+        stop_id = zespol[i]
+        stop_nr = slupek[i]
+        lines = list(api.dbtimetable_get({'busstopId': str(stop_id), 'busstopNr': str(stop_nr)}))
+        line_per_busstop_dict[(stop_id, stop_nr)] = lines
+    return line_per_busstop_dict
+
+
+def get_busstops_per_line(lines_per_busstop_dict):
+    """Returns a dictionary with lines as keys and busstops (in a tuple (id, nr)) it passes as values
+    Args:
+        lines_per_busstop_dict (dictionary): dictionary returned by get_lines_per_busstop with busstops as keys and lines as values
+    Returns:
+        dictionary with lines as keys and busstops (in a tuple (id, nr)) the line passes as values
+    """
+    busstops_per_line_dict = defaultdict(list)
+    for busstop in lines_per_busstop_dict:
+        stop_id = busstop[0]
+        stop_nr = busstop[1]
+        for line in lines_per_busstop_dict[busstop]:
+            busstops_per_line_dict[line].append((stop_id, stop_nr))
+    return dict(busstops_per_line_dict)
+
+def get_timetable_per_busstop_per_line(busstops_per_line_dict):
+    """Returns a dictionary that shows the timetable for each line at each busstop.
+    Args:
+        busstops_per_line_dict (dictionary): dictionary with lines as keys and busstops (in a tuple (id, nr)) that the line passes as values
+    Returns:
+        dictionary with as keys tuples of (line, (stop_id, stop_nr)) and as values the corresponding timetable
+    (Takes some time: ~7500 api requests)
+    """
+    timetable_per_busstop_per_line_dict = dict()
+    progress = 0
+    for line in busstops_per_line_dict:
+        busstops = busstops_per_line_dict[line]
+        for stop in busstops:
+            stop_id = stop[0]
+            stop_nr = stop[1]
+            timetable = api.dbtimetable_get({'busstopId': stop_id, 'busstopNr': stop_nr, 'line': line})
+            timetable_per_busstop_per_line_dict[(line, (stop_id, stop_nr))] = timetable
+            progress += 1
+            print(progress)
+    return timetable_per_busstop_per_line_dict
